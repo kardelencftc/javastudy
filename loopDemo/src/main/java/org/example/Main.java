d123bc10db1b02e2073ffd064e15e7cea7931fab
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-14 02:38:17 +0300

    loopDemo
    For döngüsü

---
+++ loopDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // For Döngüsü
+        int i;
+        for (i = 2; i < 10; i += 2) {
+            
+        }
+        System.out.println(i);
+        System.out.println("Döngü bitti");
+    }
+}
