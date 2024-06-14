1abfa480e4ea372751caf400b8e989fdc3bcff38
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-14 03:02:17 +0300

    LoopDöngüsü

--- loopDemo/src/main/java/org/example/Main.java
+++ loopDemo/src/main/java/org/example/Main.java
@@ -6,9 +6,25 @@ public class Main {
         // For Döngüsü
         int i;
         for (i = 2; i < 10; i += 2) {
-            
+            // Döngü içeriği boş bırakıldı
         }
-        System.out.println(i);
-        System.out.println("Döngü bitti");
+        System.out.println(i); //
+        System.out.println("For Döngüsü bitti");
+
+        // While Döngüsü
+        i = 2;
+        while (i < 10) {
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("While Döngüsü Bitti");
+
+        // Do-While döngüsü
+        int j = 0;
+        do {
+            System.out.println(j);
+            j += 2;
+        } while (j < 100);
+        System.out.println("Do-While Döngüsü Bitti");
     }
 }
