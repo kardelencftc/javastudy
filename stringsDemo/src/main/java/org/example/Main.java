2e37faeb224d203de2f1403495ed25560363b95c
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-24 02:40:14 +0300

    stringsDemo

--- stringsDemo/src/main/java/org/example/Main.java
+++ stringsDemo/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String mesaj = "Bugün hava çok güzel.";
@@ -17,5 +19,18 @@ public class Main {
 
         System.out.println(mesaj.indexOf("av"));
         System.out.println(mesaj.indexOf("a"));
+
+        String yeniMesaj = mesaj.replace(' ', '-');
+        System.out.println(yeniMesaj);
+
+        System.out.println(mesaj.substring(2, 5));
+
+        for (String kelime : mesaj.split(" ")) {
+            System.out.println(kelime);
+        }
+
+        System.out.println(mesaj.toLowerCase());
+        System.out.println(mesaj.toUpperCase());
+        System.out.println(mesaj.trim());
     }
 }
