97ce2282a35493aca0224a14396cf5c5c95dd466
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-24 21:06:25 +0300

    methods2

--- methods2/src/main/java/org/example/Main.java
+++ methods2/src/main/java/org/example/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         String mesaj = "Bugün hava çok güzel";
         String yeniMesaj = mesaj.substring(0, 2);
-        System.out.println(yeniMesaj);
+        System.out.println(yeniMesaj); // Sonuç: Bu
 
         int sayi = topla(5, 7);
-        System.out.println(sayi);
+        System.out.println(sayi); // Sonuç: 12
+
+        int toplam = topla2(2, 3, 4, 5, 6, 10); // Doğru parametre geçişi
+        System.out.println(toplam); // Çoklu toplama sonucu
 
         // Diğer metodları çağırma
         ekle();
@@ -35,14 +38,15 @@ public class Main {
         return sayi1 + sayi2; // Doğru toplama işlemi
     }
 
+    public static int topla2(int... sayilar) {
+        int toplam = 0;
+        for (int sayi : sayilar) {
+            toplam += sayi;
+        }
+        return toplam; // Toplam sonucu döndür
+    }
+
     public static String sehirVer() {
         return "Ankara"; // Geri dönüş değeri eklenmeli
     }
 }
-
-
-
-
-
-
-
