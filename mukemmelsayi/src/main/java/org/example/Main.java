03fb7d1f9c8a623eb08175c716e5e1f126a5e8fb
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-24 03:46:53 +0300

    mukemmelsayi

---
+++ mukemmelsayi/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //6-->1,2,3
+        int number = 5;
+        int total = 0;
+         for (int i=1;i<number;i++){
+             if(number % i ==0){
+                 total = total +i ;
+             }
+             if (total == number){
+                 System.out.println("Mükemmel sayıdır");
+             }else{
+                 System.out.println("Mükemmel sayı değildir");
+             }
+         }
+    }
+}
