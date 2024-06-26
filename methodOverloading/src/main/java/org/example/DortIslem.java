fd030d55523d569b6ad0f7db9ad3641b61c5fd9b
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-26 17:15:21 +0300

    classesWithAttributes

---
+++ methodOverloading/src/main/java/org/example/DortIslem.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DortIslem {
+    // İlk topla metodu, iki tamsayıyı toplar ve sonucu döner
+    public int topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    // İkinci topla metodu, üç tamsayıyı toplar ve sonucu döner
+    public int topla(int sayi1, int sayi2, int sayi3) {
+        return sayi1 + sayi2 + sayi3;
+    }
+}
