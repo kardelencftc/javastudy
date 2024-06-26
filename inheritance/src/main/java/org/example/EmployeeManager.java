52609fd0a4c3ff8fcf2cff84a6314d5cf96d6542
Kardelen Çiftçi <kardelencftc98@gmail.com>
2024-06-26 18:22:26 +0300

    inheritance

---
+++ inheritance/src/main/java/org/example/EmployeeManager.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class EmployeeManager extends PersonManager{
+}
---
+++ inheritance/src/main/java/org/example/Person.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Person {
+    int id;
+    String firstName;
+    String lastname;
+    int age;
+
+}
+
